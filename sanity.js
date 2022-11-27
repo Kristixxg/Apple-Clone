6da1364a4de21e374d5beca9b218e961f02c8cc4
Kristy Guo <krisxxg@gmail.com>
2022-11-27 00:54:27 -0800

    test sanity project id using string

--- sanity.js
+++ sanity.js
@@ -4,7 +4,7 @@ import createImageUrlBuilder from "@sanity/image-url";
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   token: process.env.SANITY_API_TOKEN,
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: "0p2p3xzm",
   apiVersion: "2021-10-21", // Learn more: https://www.sanity.io/docs/api-versioning
   useCdn: process.env.NODE_ENV === "production",
 };
