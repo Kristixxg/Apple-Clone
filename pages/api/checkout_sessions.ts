09be8b9c9b8ff02379cb83d249d3215ba7c41c34
Kristy Guo <krisxxg@gmail.com>
2022-11-27 12:55:27 -0800

    stripe api key to string

--- pages/api/checkout_sessions.ts
+++ pages/api/checkout_sessions.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 import { urlFor } from "../../sanity";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`!, {
   // https://github.com/stripe/stripe-node#configuration
   apiVersion: "2022-11-15",
 });
